a78dc25da0123881ccb240c07789f65264019427
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-06 00:27:49 +0100

    Calculator - While Loops, Flags & Recursion

--- main.py
+++ main.py
@@ -23,19 +23,24 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number? "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number? "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
+def calculator():
+  num1 = int(input("What's the first number? "))
+  for symbol in operations:
+      print(symbol)
+  should_continue = True
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = int(input("What's the next number? "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
 
-operation_symbol = input("Pick another operation:")
-num3 = int(input("What's the next number? "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(calculation_function(num1, num2), num3)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} = {operation_symbol} {num2} = {second_answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+        num1 = answer
+    else:
+        should_continue = False
+        calculator()
+
+calculator()
